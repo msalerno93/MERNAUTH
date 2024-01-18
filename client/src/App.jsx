8d2b4cfc34af35696b9e44e60f6c7fc6870b1306
Michael Salerno <msalerno1993@gmail.com>
2024-01-17 20:32:15 -0500

    create navbar component with functional routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="bg-slate-400">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">MERN AUTH APP</h1>
+        </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+
+          <Link to="/sign-in">
+            <li>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
