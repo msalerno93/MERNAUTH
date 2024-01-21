d720e3f195311e78ee2a6b51eac5be64b49d92d3
Michael Salerno <msalerno1993@gmail.com>
2024-01-21 16:54:58 -0500

    set cookie expiry date

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,7 +26,11 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(404, "Invalid Credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
